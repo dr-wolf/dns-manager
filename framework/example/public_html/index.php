b7d37dd8a5e93b9c3d910cb2674f0c5848b8a942
Taras "Dr.Wolf" Supyk <dr-wolf@users.noreply.github.com>
2017-03-16 20:06:20 +0200

    Update index.php

--- framework/example/public_html/index.php
+++ framework/example/public_html/index.php
@@ -1,6 +1,6 @@
 <?php    
     define('APP_PATH', __DIR__.'/../application/');
-    define('FRAMEWORK_PATH', '/home/www-data/frameworks/mosquito/');
+    define('FRAMEWORK_PATH', __DIR__'/../../');
 
     require_once(FRAMEWORK_PATH.'core.php');
 
